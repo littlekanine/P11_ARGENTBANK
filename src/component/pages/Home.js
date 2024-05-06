95326a3e6bdd3e614e671372f17e9e36dec8797e
littlekanine <elia.kopff3891@gmail.com>
2024-05-06 16:16:02 +0200

    first modification

---
+++ src/component/pages/Home.js
@@ -0,0 +1,13 @@
+import Layout from "../layout/layout";
+
+function Home () {
+    return (
+        <div>
+            <Layout>
+
+            </Layout>
+        </div>
+    )
+}
+
+export default Home
