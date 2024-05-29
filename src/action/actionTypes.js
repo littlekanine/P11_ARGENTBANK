ae0926d04612b2a80c12005d4d98639134e8b806
littlekanine <elia.kopff3891@gmail.com>
2024-05-29 09:38:38 +0200

    fix bug

--- src/action/actionTypes.js
+++
@@ -1,6 +0,0 @@
-export const STORE_TOKEN = (token) => ({
-    type: 'STORE_TOKEN', //type de l'action : Nature du changement à effectué
-    payload: token
-  });
-
-export const LOGOUT = 'LOGOUT';
--- src/component/form/login/FormLogin.js
+++ src/component/form/login/FormLogin.js
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { setUser, storeToken } from '../../../feature/userSlice';
-import { Form, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { loginUser, usersData } from '../../../feature/UserActions';
 import './formLogin.css';
 
